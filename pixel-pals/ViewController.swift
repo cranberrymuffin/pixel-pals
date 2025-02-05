f655fad0dadec90e32f1be1603e9d28bc4c78671
cranberrymuffin <aparnalovestocode@gmail.com>
2025-02-05 13:45:25 -0500

    Initial Commit

---
+++ pixel-pals/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pixel-pals
+//
+//  Created by Aparna Natarajan on 2/5/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
